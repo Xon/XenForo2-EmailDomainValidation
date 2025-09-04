c4d7e60fd9b7797c3ef33537e69a569b3d841608
Xon <635541+Xon@users.noreply.github.com>
2025-09-04 21:19:33 +0800

    Xf2.3 export

---
+++ upload/src/addons/SV/EmailDomainValidation/_output/option_hint.php
@@ -0,0 +1,18 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE OPTIONS IN THE CONTROL PANEL.
+
+/**
+ * @noinspection PhpMultipleClassDeclarationsInspection
+ * @noinspection PhpIllegalPsrClassPathInspection
+ */
+
+namespace XF;
+
+/**
+ * @property bool|null $svAdminBypassEmailDnsChecks Admin user edits skip additional DNS-validation of email domains
+ */
+class Options
+{
+}
