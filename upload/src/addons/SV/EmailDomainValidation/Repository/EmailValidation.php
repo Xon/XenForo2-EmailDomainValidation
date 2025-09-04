4c3b39e30e34d3cea5283e17c3ff703c640315ef
Xon <635541+Xon@users.noreply.github.com>
2025-09-05 05:34:26 +0800

    Signup abuse detection & blocking support

--- upload/src/addons/SV/EmailDomainValidation/Repository/EmailValidation.php
+++ upload/src/addons/SV/EmailDomainValidation/Repository/EmailValidation.php
@@ -2,9 +2,16 @@
 
 namespace SV\EmailDomainValidation\Repository;
 
+use SV\SignupAbuseBlocking\Globals;
+use SV\SignupAbuseBlocking\Spam\Checker\User\Email as EmailSpamCheck;
+use SV\SignupAbuseBlocking\Spam\Checker\User\EmailDomain as EmailDomainSpamCheck;
 use SV\StandardLib\Helper;
 use XF\Entity\AdminNavigation as AdminNavigationEntity;
+use XF\Entity\User as UserEntity;
 use XF\Mvc\Entity\Repository;
+use XF\Repository\User as UserRepo;
+use XF\Spam\Checker\AbstractProvider;
+use XF\Spam\UserChecker;
 use XF\Validator\Email as EmailValidator;
 
 class EmailValidation extends Repository
@@ -37,6 +44,57 @@ class EmailValidation extends Repository
         $redisInfo->saveIfChanged();
     }
 
+    protected function getEmailSpamCheckersForSignup(): array
+    {
+        if (Helper::isAddOnActive('SV/SignupAbuseBlocking'))
+        {
+            return [
+                EmailSpamCheck::class,
+                EmailDomainSpamCheck::class,
+            ];
+        }
+
+        return [];
+    }
+
+    protected function checkEmailForSignup(string $email, array &$errors): void
+    {
+        $signupCheckers = $this->getEmailSpamCheckersForSignup();
+
+        if (count($signupCheckers) === 0)
+        {
+            return;
+        }
+
+        $app = \XF::app();
+        $guestUser = Helper::repository(UserRepo::class)->getGuestUser(null, function (array $data) use ($email) {
+            $data['email'] = $email;
+
+            return $data;
+        });
+
+        foreach ($signupCheckers as $class)
+        {
+            if (class_exists($class))
+            {
+                $checker = Helper::newExtendedClass(UserChecker::class, $app);
+                $provider = Helper::newExtendedClass($class, $checker, $app);
+
+                $this->doCheckEmailForSignup($provider, $guestUser);
+
+                foreach ($checker->getDetails() as $row)
+                {
+                    $errors[] = \XF::phrase($row['phrase'], $row['data']);
+                }
+            }
+        }
+    }
+
+    protected function doCheckEmailForSignup(AbstractProvider $provider, UserEntity $guestUser): void
+    {
+        $provider->check($guestUser);
+    }
+
     public function validateEmailAddress(string $email, string &$formatedEmail, array &$signupErrors, array &$errors, array &$warnings): bool
     {
         $signupErrors = $errors = $warnings = [];
@@ -45,10 +103,13 @@ class EmailValidation extends Repository
         $emailValidator = $app->validator(EmailValidator::class);
         $emailValidator->setOption('banned', $app->container('bannedEmails'));
         $emailValidator->setOption('check_typos', true);
-        $emailValidator->setOption('check_typos', true);
+        $emailValidator->setOption('allow_local', false);
         $emailValidator->setOption('dns_validate', true);
         $emailValidator->setOption('sv_extended_error', true);
         $formatedEmail = $emailValidator->coerceValue($email);
+
+        $this->checkEmailForSignup($formatedEmail, $signupErrors);
+
         /** @var array<string,string>|string|null $errorKey */
         $valid = $emailValidator->isValid($formatedEmail, $errorKey);
         if ($valid)
