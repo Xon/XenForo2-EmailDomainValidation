ba8a60698fbf12f91b4013ee1272bcbe170d9391
Xon <635541+Xon@users.noreply.github.com>
2025-09-05 13:03:28 +0800

    Improve typo-detection, and do it before banned email checks

--- upload/src/addons/SV/EmailDomainValidation/XF/Validator/Email.php
+++ upload/src/addons/SV/EmailDomainValidation/XF/Validator/Email.php
@@ -6,7 +6,12 @@ use SV\EmailDomainValidation\EmailValidation as XFEmailValidation;
 use SV\EmailDomainValidation\Globals;
 use SV\StandardLib\Helper;
 use XF\Repository\Banning as BanningRepo;
+use function array_key_exists;
 use function is_string;
+use function mb_strtolower;
+use function strlen;
+use function strrpos;
+use function substr;
 
 /**
  * @Extends \XF\Validator\Email
@@ -23,8 +28,29 @@ class Email extends XFCP_Email
     /** @noinspection PhpMissingReturnTypeInspection */
     public function isValid($value, &$errorKey = null)
     {
-        $isValid = parent::isValid($value, $errorKey);
         $options = $this->options;
+        // move typo-detection *before* other checks (including banned email address check)
+        $checkTypos = $options['check_typos'];
+        if ($checkTypos && $value !== '')
+        {
+            $this->options['check_typos'] = false;
+            if ($this->hasTypos($value, $errorKey))
+            {
+                return false;
+            }
+        }
+
+        try
+        {
+            $isValid = parent::isValid($value, $errorKey);
+        }
+        finally
+        {
+            if ($checkTypos)
+            {
+                $this->options['check_typos'] = true;
+            }
+        }
 
         if ($value === '' || !$isValid)
         {
@@ -47,11 +73,6 @@ class Email extends XFCP_Email
         return true;
     }
 
-    /**
-     * @param string $email
-     * @param array|string|null &$errorKey
-     * @return bool
-     */
     protected function hasInvalidDNS(string $email, &$errorKey): bool
     {
         $options = $this->options;
@@ -79,11 +100,170 @@ class Email extends XFCP_Email
         }
         else
         {
+            /** @noinspection SpellCheckingInspection */
             $errorKey = ($emailValidation->warnings[XFEmailValidation::BANNED_EMAIL] ?? false)
                 ? 'banned'
                 : 'invaliddomain';
         }
 
         return true;
+    }
+
+    /** @noinspection SpellCheckingInspection */
+    protected function getTldPermutations(): array
+    {
+        // should be all lowercase
+        return [
+            '.com' => [
+                '.com.com',
+                '.cmo',
+                '.cm',
+                '.co'
+            ],
+            '.co.fr' => [
+                '.cofr',
+                '.co.ft.com',
+            ],
+            '.co.br' => [
+                '.couk',
+                '.co.br.com',
+            ],
+            '.co.uk' => [
+                '.couk',
+                '.co.uk.com',
+            ],
+            '.co.jp' => [
+                '.couk',
+                '.co.jp.com',
+            ],
+        ];
+    }
+
+    /** @noinspection SpellCheckingInspection */
+    protected function getDomainsPermutations(): array
+    {
+        // This is a very basic function and is really just trying to catch simple typos.
+        // Most significantly, gamil.com since this can trigger an SFS action unexpectedly.
+        return [
+            'gmail' => [
+                'gamil',
+                'gmial',
+                'gnail',
+                'gmai',
+                'gnai',
+            ],
+            'outlook' => [
+                'outlok',
+                'otlook',
+                'oultook',
+                'outloook',
+                'outlo',
+                'outloo',
+                'ooutlook',
+            ],
+            'hotmail' => [
+                'hotnail',
+                'hotmai',
+                'hotnai',
+            ],
+            'live' => [
+                'livee',
+                'liveee',
+                'lvie',
+                'liev',
+                'windows',
+                'window',
+            ],
+            'yahoo' => [
+                'yaho',
+                'yahooo',
+                'yaaho',
+                'yaahoo',
+            ],
+        ];
+    }
+
+    /** @var array<string,string>|null */
+    protected static $misspelledDomains = null;
+
+    protected function getBasicMisspelledDomains(): array
+    {
+        if (self::$misspelledDomains !== null)
+        {
+            return self::$misspelledDomains;
+        }
+
+        $misspelledDomains = [];
+        $topLevelDomains = $this->getTldPermutations();
+        $domains = $this->getDomainsPermutations();
+        foreach ($domains as $domain => $domainPermutations)
+        {
+            foreach ($topLevelDomains as $tld => $tldPermutations)
+            {
+                $host = $domain . $tld;
+                foreach ($domainPermutations as $domainPermutation)
+                {
+                    $misspelledDomains[$domainPermutation . $tld] = $host;
+                }
+                foreach ($tldPermutations as $tldPermutation)
+                {
+                    $misspelledDomains[$domain . $tldPermutation] = $host;
+                    foreach ($domainPermutations as $domainPermutation)
+                    {
+                        $misspelledDomains[$domainPermutation . $tldPermutation] = $host;
+                    }
+                }
+            }
+        }
+
+        self::$misspelledDomains = $misspelledDomains;
+
+        return self::$misspelledDomains;
+    }
+
+    /**
+     * @param string $email
+     * @param array|string|null &$errorKey
+     * @return bool
+     */
+    protected function hasTypos(string $email, &$errorKey): bool
+    {
+        $lastAtPos = strrpos($email, '@');
+        if ($lastAtPos === false)
+        {
+            return false;
+        }
+
+        $domain = substr($email, $lastAtPos + 1);
+        if ($domain === '')
+        {
+            return false;
+        }
+
+        $domain = mb_strtolower($domain);
+        if ($domain[strlen($domain) - 1] === '.')
+        {
+            $domains = [$domain => substr($domain, 0, -1)];
+        }
+        else
+        {
+            $domains = $this->getBasicMisspelledDomains();
+        }
+        if (array_key_exists($domain, $domains))
+        {
+            $options = $this->options;
+            if ($options['sv_extended_error'] ?? false)
+            {
+                $errorKey = ['typo_entry' => ['entry' => $domains[$domain]]];
+            }
+            else
+            {
+                $errorKey = 'typo';
+            }
+
+            return true;
+        }
+
+        return false;
     }
 }
